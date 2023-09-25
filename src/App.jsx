db55aeac8abbfae9d32ec251b80282db561c120f
Meraj Faizan <mdmaraj1500@gmail.com>
2023-09-25 12:27:11 +0600

    fake data added and fetched successfully on home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("donation.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <main>
       <h1 className="text-5xl capitalize text-center">
