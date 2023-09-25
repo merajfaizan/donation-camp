3892f73eaf1bbbe2c5819136237d0590f323a773
Meraj Faizan <mdmaraj1500@gmail.com>
2023-09-25 10:53:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-5xl capitalize text-center">
+        project setup initialized
+      </h1>
+    </main>
+  );
+}
+
+export default App;
